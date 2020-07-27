2953418df60734e769d746b80a8d3e93ab482d76
Ziadalfian <muhammadziadalfian07@gmail.com>
2020-07-28 06:26:44 +0800

    const

---
+++ constant.dart
@@ -0,0 +1,12 @@
+void main() {
+  // keyword final
+  final name = "dwi Pramana";
+  print(name);
+
+  // keyword const
+  const number = 1000;
+  const double multiplyNumber = 1.5 * number;
+
+  print(number);
+  print(multiplyNumber);
+}
