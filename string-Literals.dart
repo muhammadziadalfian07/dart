a59cd8fb0ba06033659cdad5769d4cce1fd81ee5
Ziadalfian <muhammadziadalfian07@gmail.com>
2020-07-28 06:18:00 +0800

    string literal dan interpolation

---
+++ string-Literals.dart
@@ -0,0 +1,9 @@
+void main() {
+  //  string literal
+  String nama = "dwi";
+  String myLastName = "pratama";
+  String today = "jum'at";
+
+  // string interpolation
+  print("Nama saya adalah $nama $myLastName. saya ulang tahun hari $today");
+}
